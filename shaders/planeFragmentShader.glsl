cd4c4d95fd7a2c6274520a1edf902db59c5f7170
Kamil Kamiński <kkaminski9311@gmail.com>
2017-05-04 23:08:05 +0200

    fragment shader refactor

--- shaders/planeFragmentShader.glsl
+++ shaders/planeFragmentShader.glsl
@@ -8,7 +8,6 @@ out vec4 color;
 
 uniform int level;
 uniform isampler2D heightmap;
-uniform sampler2D testTex;
 uniform bool debug = true;
 uniform int meshSize;
 
@@ -18,18 +17,20 @@ vec4 blue = vec4(0, 0, 1, 1);
 vec4 yellow = green + red;
 
 void main() {
-    if(debug) {
-        if(level % 4 == 0) color = red;
-        else if(level % 4 == 1) color = green;
-        else if(level % 4 == 2) color = blue;
-        else color = yellow;
-        if(morphFactor > 0)
-            color = mix(color, vec4(1, 1, 1, 1), morphFactor);
-        if(sample_ < -32000.0) color = vec4(0, 0, 0, 1);
-        // color = mix(color, texture(testTex, poss.xz / meshSize + vec2(0.5, 0.5)), 0.5);
-        // 1249 to max height
+    if(level % 4 == 0) color = red;
+    else if(level % 4 == 1) color = green;
+    else if(level % 4 == 2) color = blue;
+    else color = yellow;
+    if(morphFactor > 0) color = mix(color, vec4(1, 1, 1, 1), morphFactor);
+    if(sample_ < -32000.0) color = vec4(0, 0, 0, 1);
+    if(debug)
+        // 1249 to max height na N50E016
+        color = mix(
+            color, 
+            vec4(sample_ / 1249.0f, sample_ / 1249.0f, 0, 1), 
+            0.7
+        );
+    else {
         color = vec4(sample_ / 1249.0f, .1, .1, 1);
-    } else {
-        color = mix(vec4(0, 1, 0, 1), vec4(1, 1, 1, 1), abs(sample_) / 8000.0);
     }
 }
